6be0921f62091a227a5e1ea8bc59e190e786e5d3
anaritacanelas <ritacanelasferreira@gmail.com>
2022-06-26 19:33:33 +0100

    mobile navigation

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import Modal from "react-modal";
+
 import "./Nav.css";
 
 import logo from "../images/summercamp-logo.svg";
@@ -11,79 +13,93 @@ export default function Nav() {
   }
 
   return (
-    <nav className="nav">
-      <div className="container">
-        <div className="nav-content">
-          <div className="brand">
-            <a href="/">
-              <img src={logo} alt="" />
-            </a>
-          </div>
-          <div className="menu">
-            <a className="has-font-size-small has-text-black" href="#projects">
-              Projects
-            </a>
-            <a
-              className="has-font-size-small has-text-black"
-              href="#how-it-works"
-            >
-              How it works
-            </a>
-            <a
-              className="has-font-size-small has-text-black"
-              href="#testimonials"
-            >
-              Testimonials
-            </a>
-            <a className="has-font-size-small has-text-black" href="#faqs">
-              FAQ
-            </a>
-            <a href="/" className="button is-small">
-              Join Us
-            </a>
-          </div>
-          <div className="toggle" onClick={toggleMenu}>
-            toggle
+    <>
+      <nav className="nav">
+        <div className="container">
+          <div className="nav-content">
+            <div className="brand">
+              <a href="/">
+                <img src={logo} alt="" />
+              </a>
+            </div>
+            <div className="menu">
+              <a
+                className="has-font-size-small has-text-black"
+                href="#projects"
+              >
+                Projects
+              </a>
+              <a
+                className="has-font-size-small has-text-black"
+                href="#how-it-works"
+              >
+                How it works
+              </a>
+              <a
+                className="has-font-size-small has-text-black"
+                href="#testimonials"
+              >
+                Testimonials
+              </a>
+              <a className="has-font-size-small has-text-black" href="#faqs">
+                FAQ
+              </a>
+              <a href="/" className="button is-small">
+                Join Us
+              </a>
+            </div>
+            <div className="toggle" onClick={toggleMenu}></div>
           </div>
         </div>
-      </div>
-      <div
-        className={
-          "mobile-menu has-background-blue has-text-white" +
-          (isMenuVisible ? "" : " is-hidden")
-        }
+      </nav>
+      <Modal
+        isOpen={isMenuVisible}
+        onRequestClose={toggleMenu}
+        contentLabel="Menu Modal"
+        className="mobile-menu has-background-blue has-text-white"
+        overlayClassName="mobile-menu-overlay"
       >
-        <div className="brand">Summer Camp</div>
-        <div className="has-font-size-large">
-          <div className="menu-item">
-            <a
-              className="has-text-white"
-              onClick={toggleMenu}
-              href="#testimonials"
-            >
-              Testimonials
+        <nav className="nav">
+          <div className="brand">Summer Camp</div>
+          <div className="has-font-size-large">
+            <div className="menu-item">
+              <a
+                className="has-text-white"
+                onClick={toggleMenu}
+                href="#testimonials"
+              >
+                Testimonials
+              </a>
+            </div>
+            <div className="menu-item">
+              <a
+                className="has-text-white"
+                onClick={toggleMenu}
+                href="#projects"
+              >
+                Projects
+              </a>
+            </div>
+            <div className="menu-item">
+              <a className="has-text-white" onClick={toggleMenu} href="#faqs">
+                FAQs
+              </a>
+            </div>
+          </div>
+          <div>
+            <a href="/" className="button has-font-size-small has-text-white">
+              Join us
             </a>
           </div>
-          <div className="menu-item">
-            <a className="has-text-white" onClick={toggleMenu} href="#projects">
-              Projects
-            </a>
-          </div>
-          <div className="menu-item">
-            <a className="has-text-white" onClick={toggleMenu} href="#faqs">
-              FAQs
-            </a>
-          </div>
-        </div>
-        <div>
-          <a href="/" className="button has-font-size-small has-text-white">
-            Join us
-          </a>
-        </div>
-        <div className="close" onClick={toggleMenu}>
-          &times;
-        </div>
-      </div>
-    </nav>
+          <button
+            className="close"
+            aria-label="Close modal"
+            onClick={toggleMenu}
+          >
+            &times;
+          </button>
+        </nav>
+      </Modal>
+    </>
   );
 }
